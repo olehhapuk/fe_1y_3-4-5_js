de14d88243eed2de9b7b9ebb9ae8fa69cca58f0a
olehhapuk <oleggapuk20@gmail.com>
2022-04-10 19:31:03 +0300

    data types and variables

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Variables
+/*
+let b = 10;
+b = 8;
+const a = b * 2;
+
+console.log('a');
+console.log(a);
+console.log(window);
+
+let empty;
+console.log(empty);
+*/
+
+const num = 15;
+const str = 'Hello, World!';
+// \n - New line
+
+let bool = true;
+bool = false;
+
+const nothing = null;
+
+let undef;
+
+console.log(typeof num);
+
+const myAge = 21;
