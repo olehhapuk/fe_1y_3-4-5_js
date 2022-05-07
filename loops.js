8c428eb9de7cc0b9dc23de23c23ed9be5c18903e
olehhapuk <oleggapuk20@gmail.com>
2022-05-07 14:38:50 +0300

    while, do while

---
+++ loops.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// let counter = 15;
+// while (counter < 15) {
+//   console.log(counter);
+//   counter++;
+// }
+
+let counter = 15;
+do {
+  console.log(counter);
+  counter++;
+} while (counter < 15);
+
+console.log(counter);
