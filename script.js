ef929207349ca6cc8e76ca1a79a1b4011c211cec
olehhapuk <oleggapuk20@gmail.com>
2022-05-21 14:22:35 +0300

    finished array basics

--- script.js
+++ script.js
@@ -1,8 +1,65 @@
-const users = ['john', 'ben', 'peter', 'vlad'];
+const users = ['john', 'ben', 'peter', 'vlad1', 'mark'];
 
-console.log(users.length);
-console.log(users[1]);
+// console.log(users.length);
+// console.log(users[2]);
 
-for (let i = 0; i < users.length; i++) {
-  console.log(users[i]); // users[0] users[1] users[2]
-}
+// users[1] = 'vlad';
+// users[5] = 'rostyslav';
+// users[users.length] = 'ivan';
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log(users[i]);
+// }
+
+// for (const userItem of users) {
+//   console.log(userItem);
+// }
+
+// const search = prompt("Введіть ім'я");
+// let counter = 0;
+
+// for (const userItem of users) {
+//   if (userItem !== search) {
+//     counter++;
+//   }
+// }
+
+// if (counter === users.length) {
+//   console.log('Такого елемента немає');
+// } else {
+//   console.log('Такий елемент є');
+// }
+
+// const search = prompt("Введіть ім'я");
+// let result = null;
+
+// for (const userItem of users) {
+//   if (userItem === search) {
+//     result = userItem;
+//     break;
+//   }
+// }
+
+// if (result === null) {
+//   console.log('Такого елемента немає');
+// } else {
+//   console.log('Такий елемент є', result);
+// }
+
+// const coordinates = [
+//   [1, 2, 3],
+//   [2, 9, 3],
+//   [4, 2, 3],
+// ];
+
+// for (let x = 0; x < coordinates.length; x++) {
+//   for (let y = 0; y < coordinates[x].length; y++) {
+//     console.log(coordinates[x][y]);
+//   }
+// }
+
+// const figures = [
+//   [true, false, false],
+//   [false, true, true],
+//   [false, false, true],
+// ];
