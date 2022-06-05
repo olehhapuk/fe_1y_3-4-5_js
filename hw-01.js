62e539a4f642b5dccba74b568df0fcf8b51b0f04
olehhapuk <oleggapuk20@gmail.com>
2022-06-05 14:24:12 +0300

    Practice

--- hw-01.js
+++ hw-01.js
@@ -1,14 +1,48 @@
-let total = 0;
-let input;
+// --- Do while ---
+// let total = 0;
+// let input;
 
-do {
-  input = prompt('Введіть число');
-  if (isNaN(input)) {
-    alert('Було написано не число, спробуйте ще раз');
-    continue;
-  }
+// do {
+//   input = prompt('Введіть число');
+//   if (isNaN(input)) {
+//     alert('Було написано не число, спробуйте ще раз');
+//     continue;
+//   }
 
-  total += Number(input);
-} while (input !== null);
+//   total += Number(input);
+// } while (input !== null);
 
-alert(total);
+// alert(total);
+
+// --- For ---
+// let total = 0;
+// for (
+//   let input = prompt('Введіть число');
+//   input !== null;
+//   input = prompt('Введіть число')
+// ) {
+//   if (isNaN(input)) {
+//     alert('Було написано не число, спробуйте ще раз');
+//     continue;
+//   }
+
+//   total += Number(input);
+// }
+
+// alert(total);
+
+// --- While ---
+// let input = prompt('Введіть число');
+// let total = 0;
+// while (input !== null) {
+//   if (isNaN(input)) {
+//     alert('Було написано не число, спробуйте ще раз');
+//     input = prompt('Введіть число');
+//     continue;
+//   }
+
+//   total += Number(input);
+//   input = prompt('Введіть число');
+// }
+
+// alert(total);
