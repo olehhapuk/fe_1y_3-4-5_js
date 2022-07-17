fad25069b8751b6389b5b1aa3cfb542a660acea8
olehhapuk <oleggapuk20@gmail.com>
2022-07-17 14:29:01 +0300

    order product

--- script.js
+++ script.js
@@ -1,20 +1,65 @@
 const catalogList = document.querySelector('#productsList');
+const cartList = document.querySelector('#cartList');
 
 const productsManager = {
   catalog: [],
+  orderedProducts: [],
   setCatalog(products) {
     this.catalog = products;
     this.renderCatalog();
   },
   renderCatalog() {
+    const productElements = [];
     for (const product of this.catalog) {
-      this.createProductElement(product);
+      const newProductElem = this.createProductElement(product);
+      productElements.push(newProductElem);
     }
 
-    this.createOrderElement({
-      ...this.catalog[0],
-      quantity: 9,
+    catalogList.append(...productElements);
+  },
+  handleAddSubmit(e) {
+    e.preventDefault();
+
+    const input = e.currentTarget.querySelector('input');
+    const quantity = parseInt(input.value);
+    const productId = e.currentTarget.dataset.id;
+
+    this.orderProduct(productId, quantity);
+
+    input.value = '1';
+  },
+  orderProduct(id, quantity) {
+    let productToAdd;
+    for (const product of this.catalog) {
+      if (product.id === id) {
+        productToAdd = product;
+        break;
+      }
+    }
+
+    for (const product of this.orderedProducts) {
+      if (product.id === productToAdd.id) {
+        product.quantity += quantity;
+        this.renderOrderedProducts();
+        return;
+      }
+    }
+
+    this.orderedProducts.push({
+      ...productToAdd,
+      quantity,
     });
+    this.renderOrderedProducts();
+  },
+  renderOrderedProducts() {
+    const orderedElements = [];
+    for (const product of this.orderedProducts) {
+      const newProductElem = this.createOrderElement(product);
+      orderedElements.push(newProductElem);
+    }
+
+    cartList.innerHTML = '';
+    cartList.append(...orderedElements);
   },
   createProductElement(product) {
     const inputId = product.name + product.id;
@@ -47,7 +92,8 @@ const productsManager = {
 
     const form = document.createElement('form');
     form.className = 'product__form';
-    // TODO: add onSubmit
+    form.dataset.id = product.id;
+    form.addEventListener('submit', this.handleAddSubmit.bind(this));
 
     const formField = document.createElement('div');
     formField.className = 'form__field product__quantity-field';
@@ -77,11 +123,63 @@ const productsManager = {
 
     productElem.append(image, title, price, form);
 
-    console.log(productElem);
-    catalogList.appendChild(productElem);
+    return productElem;
   },
-  // Домашнє завдання: Зробити елемент замовлення
-  createOrderElement(product) {},
+  createOrderElement(product) {
+    const productElem = document.createElement('li');
+    productElem.className = 'product';
+
+    const image = document.createElement('img');
+    image.src = `images/${product.image}`;
+    image.alt = product.name;
+    image.className = 'product__img';
+    image.width = 168;
+    image.height = 180;
+
+    const title = document.createElement('h4');
+    title.className = 'product__title';
+    title.textContent = product.name;
+
+    const price = document.createElement('p');
+    price.className = 'product__price';
+    price.textContent = product.price;
+
+    const priceTag = document.createElement('span');
+    priceTag.className = 'product__price-tag';
+    priceTag.textContent = ' ₴';
+
+    price.appendChild(priceTag);
+
+    const quantityControls = document.createElement('div');
+    quantityControls.className = 'product__quantity-controls';
+
+    const decrementBtn = document.createElement('button');
+    decrementBtn.className = 'btn product__quantity-btn';
+    decrementBtn.type = 'button';
+    decrementBtn.textContent = '-';
+    // TODO: Add onclick
+
+    const quantity = document.createElement('span');
+    quantity.className = 'tag product__quantity';
+    quantity.textContent = product.quantity;
+
+    const incrementBtn = document.createElement('button');
+    incrementBtn.className = 'btn product__quantity-btn';
+    incrementBtn.type = 'button';
+    incrementBtn.textContent = '+';
+    // TODO: Add onclick
+
+    quantityControls.append(decrementBtn, quantity, incrementBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn';
+    deleteBtn.type = 'button';
+    deleteBtn.textContent = 'Видалити';
+
+    productElem.append(image, title, price, quantityControls, deleteBtn);
+
+    return productElem;
+  },
 };
 
 function loadProductsCatalog(callback) {
