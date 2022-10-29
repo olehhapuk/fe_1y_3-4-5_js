bf7e25b62ecac8492d3bcd4f11ac53abf8363851
olehhapuk <oleggapuk20@gmail.com>
2022-10-29 14:49:37 +0300

    Multi modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,15 +2,35 @@ const modalOpenBtn = document.querySelector('#modalOpenBtn');
 const backdropElem = document.querySelector('#modal');
 const modalCloseBtn = document.querySelector('#modalCloseBtn');
 
-modalOpenBtn.addEventListener('click', () => {
+function openModal() {
   backdropElem.classList.add('open');
-});
+}
 
-backdropElem.addEventListener('click', () => {});
-
-modalCloseBtn.addEventListener('click', () => {
+function closeModal() {
   backdropElem.classList.remove('open');
-});
+}
 
-window.addEventListener('keydown', () => {});
-s;
+function handleModalOpenBtnClick() {
+  openModal();
+}
+
+function handleBackdropElemClick(e) {
+  if (e.target === e.currentTarget) {
+    closeModal();
+  }
+}
+
+function handleModalCloseBtnClick() {
+  closeModal();
+}
+
+function handleKeydown(e) {
+  if (e.code === 'Escape') {
+    closeModal();
+  }
+}
+
+modalOpenBtn.addEventListener('click', handleModalOpenBtnClick);
+backdropElem.addEventListener('click', handleBackdropElemClick);
+modalCloseBtn.addEventListener('click', handleModalCloseBtnClick);
+window.addEventListener('keydown', handleKeydown);
