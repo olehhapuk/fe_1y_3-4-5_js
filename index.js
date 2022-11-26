43b4bf4eaeae2cdfefcfbade6317c43cf12da3b5
olehhapuk <oleggapuk20@gmail.com>
2022-11-26 14:11:12 +0200

    NodeJS

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const message = 'Hello, World!';
-console.log(message);
+// import express from 'express'; // EcmaScript Import
+
+// export default []; // EcmaScript Export
+
+// module.exports = []; // CommonJS Export
+
+const express = require('express'); // CommonJS Import
+
+const app = express();
+
+console.log('Test');
+
+app.use(express.static('./public'));
+
+app.listen(3000, () => console.log('Open http://localhost:3000'));
