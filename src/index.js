bdbcb1c4735135b8867e11d8ca4897f9fe8f2470
olehhapuk <oleggapuk20@gmail.com>
2023-01-14 12:56:23 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import './styles/index.scss';
-
-// import './scripts/randomInterval';
-// import './scripts/counter';
-// import './scripts/date';
-// import './scripts/timer';
-import { Timer } from './scripts/timer';
-
-const timer = new Timer(
-  '#startBtn',
-  '#stopBtn',
-  '#pauseBtn',
-  '#output',
-  '#notificationTime'
-);
