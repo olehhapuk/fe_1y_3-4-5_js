b938c517295932d57f51acaf3379676bf37faee1
olehhapuk <oleggapuk20@gmail.com>
2023-01-15 13:41:40 +0200

    post rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import './styles/index.scss';
 
-const str = JSON.stringify({ id: 1 });
-console.log(JSON.parse(str));
+const apiUrl = 'https://jsonplaceholder.typicode.com';
+
+fetch(apiUrl + '/posts')
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      console.error('Fetch error');
+    }
+  })
+  .then((data) => {
+    console.table(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
