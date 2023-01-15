da9c2a9380a7926f7379a3c54a3ce607fa6e7df6
olehhapuk <oleggapuk20@gmail.com>
2023-01-15 14:21:25 +0200

    loaded 1 page with posts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
 import './styles/index.scss';
+import postTemplate from './partials/post.hbs';
+
+const refs = {
+  postsList: document.querySelector('.posts-list'),
+  postsLoader: document.querySelector('#postsLoader'),
+};
 
 const apiUrl = 'https://jsonplaceholder.typicode.com';
 
-fetch(apiUrl + '/posts')
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      console.error('Fetch error');
-    }
-  })
-  .then((data) => {
-    console.table(data);
-  })
-  .catch((err) => {
-    console.log(err);
+fetchPosts();
+
+function fetchPosts() {
+  postsLoader.style.display = 'block';
+
+  // Delay loader display
+  // let timerId = setTimeout(() => {
+  //   postsLoader.style.display = 'block';
+  // }, 1000);
+
+  const page = 1;
+  const limit = 10;
+
+  fetch(apiUrl + '/posts' + `?_page=${page}&_limit=${limit}`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        console.error('Fetch error');
+      }
+    })
+    .then((data) => {
+      renderPosts(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      // clearTimeout(timerId);
+      postsLoader.style.display = 'none';
+    });
+}
+
+function renderPosts(postsData) {
+  let postsHtml = '';
+  postsData.forEach((post) => {
+    const postHtml = postTemplate({
+      title: post.title,
+      body: post.body,
+    });
+    postsHtml += postHtml;
   });
+
+  refs.postsList.insertAdjacentHTML('beforeend', postsHtml);
+}
