40144c16abab440acafcb32745acb3dd7850569e
olehhapuk <oleggapuk20@gmail.com>
2023-01-21 14:21:23 +0200

    posts infinite loading

--- src/index.js
+++ src/index.js
@@ -4,26 +4,65 @@ import postTemplate from './partials/post.hbs';
 const refs = {
   postsList: document.querySelector('.posts-list'),
   postsLoader: document.querySelector('#postsLoader'),
+  loadMoreBtn: document.querySelector('#loadMoreBtn'),
 };
 
 const apiUrl = 'https://jsonplaceholder.typicode.com';
+const limit = 27;
+let page = 1;
+let postsLoading = false;
+let totalPostsPages = 0;
+
+refs.loadMoreBtn.addEventListener('click', fetchPosts);
+
+const observer = new IntersectionObserver((entries) => {
+  const btnInfo = entries[0];
+  if (btnInfo.isIntersecting) {
+    fetchPosts();
+  }
+});
+
+observer.observe(refs.loadMoreBtn);
 
 fetchPosts();
 
+function setPostsLoading(value) {
+  if (value === true) {
+    postsLoading = value;
+    postsLoader.style.display = 'block';
+    // refs.loadMoreBtn.disabled = true;
+    refs.loadMoreBtn.style.display = 'none';
+  } else {
+    postsLoading = value;
+    postsLoader.style.display = 'none';
+    // refs.loadMoreBtn.disabled = false;
+    if (page <= totalPostsPages) {
+      refs.loadMoreBtn.style.display = 'block';
+    }
+  }
+}
+
 function fetchPosts() {
-  postsLoader.style.display = 'block';
+  if (postsLoading) {
+    return;
+  }
 
-  // Delay loader display
-  // let timerId = setTimeout(() => {
-  //   postsLoader.style.display = 'block';
-  // }, 1000);
+  setPostsLoading(true);
 
-  const page = 1;
-  const limit = 10;
+  const searchParams = new URLSearchParams();
+  searchParams.append('_page', page);
+  searchParams.append('_limit', limit);
 
-  fetch(apiUrl + '/posts' + `?_page=${page}&_limit=${limit}`)
+  const searchString = searchParams.toString();
+
+  fetch(apiUrl + '/posts?' + searchString)
     .then((res) => {
       if (res.ok) {
+        totalPostsPages = Math.ceil(+res.headers.get('x-total-count') / limit);
+        if (page > totalPostsPages) {
+          refs.loadMoreBtn.style.display = 'none';
+        }
+        page++;
         return res.json();
       } else {
         console.error('Fetch error');
@@ -36,8 +75,7 @@ function fetchPosts() {
       console.log(err);
     })
     .finally(() => {
-      // clearTimeout(timerId);
-      postsLoader.style.display = 'none';
+      setPostsLoading(false);
     });
 }
 
@@ -51,5 +89,7 @@ function renderPosts(postsData) {
     postsHtml += postHtml;
   });
 
+  // refs.postsList.innerHTML = postsHtml; // Не підходить
+  // refs.postsList.innerHTML += postsHtml; // Підходить
   refs.postsList.insertAdjacentHTML('beforeend', postsHtml);
 }
