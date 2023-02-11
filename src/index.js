cd1d5778df2382f9bdad1d7e0fe6b19097104f9e
olehhapuk <oleggapuk20@gmail.com>
2023-02-11 14:25:33 +0200

    display news

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-import './scripts/chaining';
+// import './scripts/chaining';
+
+import axios from 'axios';
+import hitTemplate from './partials/hit.hbs';
+
+// https://hn.algolia.com/api/v1/search?query=javascript&tags=story...
+
+const refs = {
+  newsList: document.querySelector('#news-list'),
+};
+
+axios
+  .get('https://hn.algolia.com/api/v1/search', {
+    params: {
+      query: 'javascript',
+      tags: 'story',
+      page: 0,
+      hitsPerPage: 12,
+    },
+  })
+  .then((res) => {
+    let html = '';
+    res.data.hits.forEach((hit) => {
+      const hitHtml = hitTemplate(hit);
+      html += hitHtml;
+    });
+    refs.newsList.innerHTML = html;
+  });
