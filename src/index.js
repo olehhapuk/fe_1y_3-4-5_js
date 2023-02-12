6cb11d288d6e7a6de89bb4f3e8ac02dea3093b7e
olehhapuk <oleggapuk20@gmail.com>
2023-02-12 15:11:05 +0200

    news search and pagination

--- src/index.js
+++ src/index.js
@@ -2,27 +2,66 @@
 
 import axios from 'axios';
 import hitTemplate from './partials/hit.hbs';
-
-// https://hn.algolia.com/api/v1/search?query=javascript&tags=story...
+import numeral from 'numeral';
 
 const refs = {
   newsList: document.querySelector('#news-list'),
+  resultsCount: document.querySelector('#resultsCount'),
+  pagesCount: document.querySelector('#pagesCount'),
+  searchForm: document.querySelector('#searchForm'),
+  loadMoreBtn: document.querySelector('#loadMoreBtn'),
 };
 
-axios
-  .get('https://hn.algolia.com/api/v1/search', {
-    params: {
-      query: 'javascript',
-      tags: 'story',
-      page: 0,
-      hitsPerPage: 12,
-    },
-  })
-  .then((res) => {
-    let html = '';
-    res.data.hits.forEach((hit) => {
-      const hitHtml = hitTemplate(hit);
-      html += hitHtml;
+let query = '';
+let page = 0;
+
+refs.searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  query = refs.searchForm.elements.query.value;
+  refs.newsList.innerHTML = '';
+  page = 0;
+  fetchNews();
+});
+
+refs.loadMoreBtn.addEventListener('click', () => {
+  fetchNews();
+});
+
+function fetchNews() {
+  refs.loadMoreBtn.disabled = true;
+  refs.loadMoreBtn.textContent = 'Loading...';
+
+  // https://hn.algolia.com/api/v1/search?query=javascript&tags=story...
+  axios
+    .get('https://hn.algolia.com/api/v1/search', {
+      params: {
+        query,
+        page,
+        tags: 'story',
+        hitsPerPage: 12,
+      },
+    })
+    .then((res) => {
+      let html = '';
+      res.data.hits.forEach((hit) => {
+        const hitHtml = hitTemplate(hit);
+        html += hitHtml;
+      });
+      refs.newsList.innerHTML += html;
+
+      refs.resultsCount.textContent = numeral(res.data.nbHits).format('0a');
+      refs.pagesCount.textContent = res.data.nbPages;
+      page++;
+
+      if (page === res.data.nbPages) {
+        refs.loadMoreBtn.classList.replace('d-block', 'd-none');
+      } else {
+        refs.loadMoreBtn.classList.replace('d-none', 'd-block');
+      }
+    })
+    .finally(() => {
+      refs.loadMoreBtn.disabled = false;
+      refs.loadMoreBtn.textContent = 'Load more...';
     });
-    refs.newsList.innerHTML = html;
-  });
+}
